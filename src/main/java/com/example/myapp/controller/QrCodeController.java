c95d2874eba4045056f2b7f78a6e16c6e072ac02
devsasmal44 <dev@coffeebeans.io>
2022-08-18 14:39:39 +0530

    Demo change

--- src/main/java/com/example/myapp/controller/QrCodeController.java
+++ src/main/java/com/example/myapp/controller/QrCodeController.java
@@ -48,7 +48,7 @@ public class QrCodeController {
         if (qrcodeService.isUniqueIdExists(id)) {
             Optional<Qrcode> optional = qrRepo.findById(id);
             Qrcode qrResponse = optional.get();
-            //if(>Instant.now().getEpochSecond())
+            //if(>Instant.now().getEpochSecond(a))
                 return "Qrcode found";
             //else
             //    return "Qrcode expired";
