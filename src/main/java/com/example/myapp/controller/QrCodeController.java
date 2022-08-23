4707b017f01aef89894ba2f205ce84974ac931d4
sanjay kumar <sanjay@coffeebeans.io>
2022-08-23 14:37:39 +0530

    add configuration file for cross enable

--- src/main/java/com/example/myapp/controller/QrCodeController.java
+++ src/main/java/com/example/myapp/controller/QrCodeController.java
@@ -23,7 +23,6 @@ public class QrCodeController {
         this.qrRepo = qrRepo;
     }
 
-    @CrossOrigin(origins = "http://localhost:8000")
     @GetMapping ("/save")
     public Qrcode saveQrcode() {
         qrcode.setUniqueId(String.valueOf(UUID.randomUUID()));
@@ -32,19 +31,16 @@ public class QrCodeController {
         return qrcodeService.saveQrcode(qrcode);
     }
 
-    @CrossOrigin(origins = "http://localhost:8000/")
     @GetMapping("/uniqueId")
     public String getUniqueId() {
         return qrcode.getUniqueId();
     }
 
-    @CrossOrigin
     @GetMapping("/list")
     public List<Qrcode> getQrcode() {
         return qrcodeService.getQrcode();
     }
 
-    @CrossOrigin
     @GetMapping("/exists")
     @ResponseBody
     public String isUniqueIdExists() {
