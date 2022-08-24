da65b67d3499718c506f8e72434878cbc0baacd4
sanjay kumar <sanjay@coffeebeans.io>
2022-08-24 12:26:22 +0530

    allow cross origin to all

--- src/main/java/com/example/myapp/controller/AttendanceController.java
+++ src/main/java/com/example/myapp/controller/AttendanceController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/attendance")
-@CrossOrigin(origins = {"http://localhost:8000", "http://localhost:8000/"})
+@CrossOrigin
 public class AttendanceController {
 
     private AttendanceService attendanceService;
